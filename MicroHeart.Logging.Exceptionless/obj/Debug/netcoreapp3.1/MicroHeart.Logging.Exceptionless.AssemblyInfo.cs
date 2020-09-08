da8abfac2d32adf7a0d7258faaf0ef24b5f77bd8
王郑 <418706636@qq.com>
2020-09-08 23:24:17 +0800

    updaet package information

--- MicroHeart.Logging.Exceptionless/obj/Debug/netcoreapp3.1/MicroHeart.Logging.Exceptionless.AssemblyInfo.cs
+++ MicroHeart.Logging.Exceptionless/obj/Debug/netcoreapp3.1/MicroHeart.Logging.Exceptionless.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("MicroHeart")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MicroHeart.Logging.Exceptionless")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MicroHeart.Logging.Exceptionless")]
-[assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("1.0.1.0")]
 
 // 由 MSBuild WriteCodeFragment 类生成。
 
