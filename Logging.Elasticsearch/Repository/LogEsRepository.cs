aab2590b656a6e56017cb8b039f038ac619f13d7
王郑 <418706636@qq.com>
2022-12-16 15:20:20 +0800

    升级包版本

--- Logging.Elasticsearch/Repository/LogEsRepository.cs
+++ Logging.Elasticsearch/Repository/LogEsRepository.cs
@@ -6,13 +6,6 @@ namespace Logging.Elasticsearch.Repository
 {
     public class LogEsRepository : BaseRepository<LogEntity>, ILogEsRepository
     {
-
-        public override string IndexName => nameof(LogEntity).ToLower();
-
-        public override int NumberOfShards => 1;
-
-        public override int NumberOfReplicas => 0;
-
         public LogEsRepository(IElasticClient client, IOptions<ElasticSearchOptions> options) : base(client, options)
         {
         }
